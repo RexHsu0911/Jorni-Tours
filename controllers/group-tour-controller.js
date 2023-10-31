9cc31ddf712130ce1dc9806153b92cbb13683f65
Rex Hsu <aexz5476@gmail.com>
2023-10-31 20:32:41 +0800

    feat: add group-tour(index) page

---
+++ controllers/group-tour-controller.js
@@ -0,0 +1,7 @@
+const groupTourController = {
+  getGroupTours: (req, res) => {
+    return res.render('group-tours')
+  }
+}
+
+module.exports = groupTourController
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('hello world!')
-})
+const groupTourController = require('../controllers/group-tour-controller')
+
+router.get('/group-tours', groupTourController.getGroupTours)
+// 設定 fallback 路由
+router.use('/', (req, res) => res.redirect('/group-tours'))
 
 module.exports = router
