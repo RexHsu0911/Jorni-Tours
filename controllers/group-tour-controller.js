002a50efdf09a33647b521d1c04d502438772df9
Rex Hsu <aexz5476@gmail.com>
2023-11-05 20:40:17 +0800

    feat: add categories navbar on group-tours(index) page

--- controllers/group-tour-controller.js
+++ controllers/group-tour-controller.js
@@ -2,19 +2,33 @@ const { GroupTour, Category } = require('../models')
 
 const groupTourController = {
   getGroupTours: (req, res, next) => {
-    return GroupTour.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
-    })
-      .then(groupTours => {
+    // 從網址上查詢參數 categoryId 是字串，先轉成 Number 再操作
+    const categoryId = Number(req.query.categoryId) || ''
+
+    return Promise.all([
+      GroupTour.findAll({
+        raw: true,
+        nest: true,
+        include: [Category],
+        where: {
+          ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值
+        }
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([groupTours, categories]) => {
         if (!groupTours) throw new Error("Group tours didn't exist!")
+        if (!categories) throw new Error("Categories didn't exist!")
 
         const result = groupTours.map(t => ({
           ...t, // ... 展開運算子
           description: t.description.substring(0, 50) // substring 截取字串
         }))
-        return res.render('group-tours', { groupTours: result })
+        return res.render('group-tours', {
+          groupTours: result,
+          categories,
+          categoryId
+        })
       })
       .catch(err => next(err))
   },
