644d93fc66b2f949a34657a5748ec4e853387508
Rex Hsu <aexz5476@gmail.com>
2023-11-10 13:53:52 +0800

    feat: add cartitem model

---
+++ models/cartitem.js
@@ -0,0 +1,27 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class CartItem extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  CartItem.init({
+    quantity: DataTypes.INTEGER,
+    cartId: DataTypes.INTEGER,
+    groupTourId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'CartItem',
+    tableName: 'CartItems',
+    underscored: true
+  })
+  return CartItem
+}
